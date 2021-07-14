773745a2ec4a282d7bffe3e898f665365730d3e3
经理 <mananger@qq.com>
2021-07-14 00:26:28 -0700

    分支开始

--- test/login.py
+++ test/login.py
@@ -2,13 +2,4 @@ num1 = 10
 
 num2 = 20 # yyd
 
-<<<<<<< HEAD
-num3 = 30 # manage
-
-num4 = 40 # manager
-
-=======
-num3 = 30 # manageo
-
-num4 = 40 # yyd
->>>>>>> 5f65e56cde21bc6d482324848c8416e18594bf30
+num3 = 30 # manager
